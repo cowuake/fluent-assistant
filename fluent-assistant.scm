6b46484eb16e37538c37cc889f4e020f5ed1f241
r-mura <riccardo_mura@yahoo.com>
2020-10-02 15:55:33 +0100

    NEW!

---
+++ fluent-assistant.scm
@@ -0,0 +1,293 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; FLUENT SCHEME ASSISTANT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Copyright 2020 Riccardo Mura
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;     1. Redistributions of source code must retain the above copyright notice,
+;;        this list of conditions and the following disclaimer.
+;;     2. Redistributions in binary form must reproduce the above copyright
+;;        notice, this list of conditions and the following disclaimer in the
+;;        documentation and/or other materials provided with the distribution.
+;;     3. Neither the name of the copyright holder nor the names of its
+;;        contributors may be used to endorse or promote products derived from
+;;        this software without specific prior written permission.
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;; POSSIBILITY OF SUCH DAMAGE.
+;;
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(require "portable-core.scm")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; GLOBAL VARIABLE DEFINITIONS
+;;             (FLUENT SPECIFIC)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Default name for generated temp files
+(define default-temp-output
+  "fluent-scheme-temp")
+
+
+;(define save-file-list
+;  (lambda ()
+;    (let exec-string
+;	(string-append "ls > " default-temp-output)
+;      (system exec-string))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; SYNTACTIC EXTENSIONS
+;;      (NOT SO REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;(define-syntax when
+;  (syntax-rules ()
+;    ((_ condition body ...)
+;     (if condition body ...))))
+
+
+;; TODO Redefine the obsolete macro system
+;(syntax-table-define system-global-syntax-table 'old-style-macro
+;  (macro (list name args body)
+;    (list 'syntax-table-define
+;	  'system-global-syntax-table
+;	  (quote name)
+;	  (list 'macro
+;		args
+;		body))))
+
+
+(syntax-table-define system-global-syntax-table 'when
+  (macro (condition . body)
+    (list 'if condition
+	  (cons 'begin body))))
+
+
+;(old-style-macro 'when
+;		 '(condition . body)
+;		 (list 'if condition
+;		       (cons 'begin body)))
+
+
+(syntax-table-define system-global-syntax-table 'unless
+  (macro (condition . body)
+    (list 'if
+	  (list 'not condition)
+	  (cons 'begin body))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;; CFD MISCELLANEA
+;;        (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;
+
+(define sst-beta 0.09)
+(define sst-beta* 0.0828)
+
+
+(define sst-turbulence-decay
+  (lambda (tu-inlet rho mu evr u x tu)
+    (- (sqrt (* (sqr tu-inlet)
+		(** (add1 (/ (* 3 rho u x sst-beta (sqr tu-inlet))
+			     (* 2 mu evr)))
+		    (- (/ sst-beta* sst-beta)))))
+       tu)))
+
+
+(define compute-transition-sst-inlet-tu
+  (lambda (rho mu evr u x tu)
+    (define sst-turbulence-decay-f
+      (lambda (tu-inlet)
+	(sst-turbulence-decay tu-inlet rho mu evr u x (/ tu 100))))
+    (let ((result ( * (car (illinois sst-turbulence-decay-f 0 100))
+		     100)))
+      (if (and (< result 100)
+	       (> result 0))
+	  result
+	  (error "Calculated an invalid number")))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; GENERIC UTILITIES
+;;   (FLUENT SPECIFIC)
+;;;;;;;;;;;;;;;;;;;;;;;
+
+(define reload-fluent-scheme
+  (lambda ()
+    (ti-menu-load-string "file read-macros fluent.scm")))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FLUENT SOLVER SETTINGS
+;;        (FLUENT SPECIFIC)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Association list of discretization schemes for the convective terms of the equations
+(define convective-discretization-schemes
+  '(
+    ("First Order Upwind"   . 0)
+    ("Second Order Upwind"  . 1)
+    ("Power Law"            . 2)
+    ("Central Differencing" . 3)
+    ("QUICK"                . 4)
+    ("Third-Order MUSCL"    . 6)))
+
+
+(define pressure-coupling-algorithms
+  '(
+    ("SIMPLE"  . 20)
+    ("SIMPLEC" . 21)
+    ("PISO"    . 22)
+    ("Coupled" . 24)))
+
+
+(define pressure-discretization-schemes
+  '(
+    ("Second Order"        . 12)
+    ("Standards"           . 10)
+    ("PRESTO!"             . 14)
+    ("Linear"              . 11)
+    ("Body Force Weighted" . 13)))
+
+
+(define set-indexed-option
+  (lambda (command target option alist)
+    (ti-menu-load-string (string-append command
+					target
+					(number->string
+					 (get-value-from-alist option alist))
+					"\n"))))
+
+
+(define set-discretization-scheme
+  (lambda (discretization-scheme target alist)
+    (set-indexed-option "solve set discretization-scheme "
+			target
+			discretization-scheme
+			alist)))
+
+
+(define set-momentum-discretization-scheme
+  (lambda (discretization-scheme)
+    (set-discretization-scheme discretization-scheme
+			       "mom "
+			       convective-discretization-schemes)))
+
+
+(define set-pressure-discretization-scheme
+  (lambda (discretization-scheme)
+    (set-discretization-scheme discretization-scheme
+			       "pressure "
+			       pressure-discretization-schemes)))
+
+
+(define set-pressure-coupling-algorithm
+  (lambda (option)
+    (set-indexed-option "solve set p-v-coupling "
+			""
+			option
+			pressure-coupling-algorithms)))
+
+
+(define set-gradient-scheme
+  (lambda (scheme)
+    (cond
+     ((equal?  scheme "Green-Gauss Node Based")
+      (ti-menu-load-string "solve set gradient-scheme \n yes"))
+     ((equal?  scheme "Green-Gauss Cell Based")
+      (ti-menu-load-string "solve set gradient-scheme \n no \n no \n yes"))
+     ((equal?  scheme "Least-Squares")
+      (ti-menu-load-string "solve set gradient-scheme \n no \n yes")))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MESH-RELATED UTILITIES
+;;       (FLUENT SPECIFIC)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define rename-surface
+  (lambda (old-name new-name)
+    (ti-menu-load-string (string-append
+			  "surface rename-surface "
+			  old-name
+			  " "
+			  new-name
+			  "\n"))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DATA IMPORT / EXPORT / TRANSLATION
+;;                    (FLUENT SPECIFIC)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define import-profile-from-raw-text
+  (lambda (file-name)
+    (let* ((raw-text (read-lines file-name))
+	   (header (string-append "(("
+				  (car raw-text)
+				  ")"))
+	   (fields (cdr raw-text))
+	   (body (string-list->string (transpose (map parse-csv-line fields)))))
+      (append-to-file (append (list header)
+			      (parse-line (string-append (substring body
+								    1
+								    (- (string-length body)
+								       1))
+							 ")")
+					  #\) #f))
+		      "profiles.scm"))))
+
+
+(define load-profiles
+  (lambda ()
+    (let ((file-name "profiles.scm"))
+      (if (file-exists? file-name)
+	  (ti-read-profile file-name)
+	  (display (string-append "Impossible to import profiles. File "
+				  file-name
+				  " not found."))))))
+
+
+
+;;;;;;;;;;;;;;;;
+;; TEST SUITE
+;; (FLUENT SPECIFIC)
+;;;;;;;;;;;;;;;;
+
+(define test-fluent-scheme
+  (lambda ()
+    (begin
+       (ti-read-case "test.cas")
+       (set-momentum-discretization-scheme "QUICK")
+       (set-pressure-discretization-scheme "PRESTO!")
+       (set-pressure-coupling-algorithm "Coupled")
+       (set-gradient-scheme "Least-Squares")
+       (load-profiles)
+       (read-lines "profile.raw")
+       (merge-strings (parse-csv-line
+		       "This,was,a,long,string,with,many,commas")
+		      "=//")
+       (import-profile-from-raw-text "profile.raw")
+       (load-profiles "profiles.scm")
+       )))
---
+++ portable-core.scm
@@ -0,0 +1,541 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; FLUENT SCHEME ASSISTANT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Copyright 2020 Riccardo Mura
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;     1. Redistributions of source code must retain the above copyright notice,
+;;        this list of conditions and the following disclaimer.
+;;     2. Redistributions in binary form must reproduce the above copyright
+;;        notice, this list of conditions and the following disclaimer in the
+;;        documentation and/or other materials provided with the distribution.
+;;     3. Neither the name of the copyright holder nor the names of its
+;;        contributors may be used to endorse or promote products derived from
+;;        this software without specific prior written permission.
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;; POSSIBILITY OF SUCH DAMAGE.
+;;
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; BASE UTILITIES
+;;       (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;
+
+(define add1
+  (lambda (x)
+    (+ x 1)))
+
+
+(define sub1
+  (lambda (x)
+    (- x 1)))
+
+
+(define range
+  (lambda (first increment last)
+    (if (<= first last)
+	(cons first (if (= first last)
+			'()
+			(range (+ first increment) increment last))))))
+
+
+(define sequence
+  (lambda (first last)
+      (range first 1 last)))
+
+
+;; TODO Assign proper name
+(define dummy
+  (lambda (x y)
+    (if (>= x y)
+       (range 0 y x)
+       (range 0 x y))))
+
+
+(define get-value-from-alist
+  (lambda (value alist)
+    (cdr (assoc value alist))))
+
+
+;(define transform-alist-keys
+;  (lambda (function alist)
+;    (
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; INTERACTION WITH THE SHELL
+;;                   (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+;;;;;;;;;;;;;;;
+;; BASE MATH
+;;  (REUSABLE)
+;;;;;;;;;;;;;;;
+
+(define recursive-op
+  (lambda (operator neutral f lower upper)
+    (if (<= lower upper)
+	(operator (f lower)
+		  (if (= lower upper)
+		      neutral
+		      (recursive-op operator neutral f (+ lower 1) upper))))))
+
+
+(define sum
+  (lambda (f lower upper)
+    (recursive-op + 0 f lower upper)))
+
+
+(define product
+  (lambda (f lower upper)
+    (recursive-op * 1 f lower upper)))
+
+
+(define factorial
+  (lambda (n)
+    (cond ((= n 0) 1)
+	  ((> n 0) (* n (factorial (- n 1))))
+	  ((< n 0) '()))))
+
+
+(define binomial-coefficient
+  (lambda (p q)
+    ( / (factorial p)
+        (* (factorial q)
+           (factorial (- p q))))))
+
+
+(define positive-integer?
+  (lambda (n)
+    (and (integer? n)
+         (positive? n))))
+
+
+(define non-negative-integer?
+  (lambda (n)
+    (or (positive-integer? n)
+        (equal? n 0))))
+
+
+(define binet
+  (lambda (n)
+    (if (non-negative-integer? n)
+        (let* ((a (sqrt 5))
+               (b (/ (+ 1 a) 2))
+               (c (/ (- 1 a) 2)))
+          (/ (- (expt b n)
+                (expt c n))
+             a)))))
+
+
+(define fibonacci-number
+  (lambda (n)
+    (binet n)))
+
+
+;; On a "good" Scheme implementation, this will lead to the correct Fibonacci
+;; sequence no matter how elements are computed - this need arbitrary precision
+;; for integers. On Fluent Scheme, computing the first 100 entries is enough in
+;; order to observe numbers with the wrong sign because of integer overflow.
+(define fibonacci-slow
+  (lambda (n)
+    (if (positive-integer? n)
+        (cond ((equal? n 1) '(0))
+              ((equal? n 2) '(0 1))
+              (else (let* ((previous
+                            (fibonacci-slow (sub1 n)))
+                           (reversed
+                            (reverse previous))
+                           (last
+                            (list-ref reversed 0))
+                           (before-last
+                            (list-ref reversed 1)))(append previous (list (+ last before-last)))))))))
+
+
+;; This exists only for showing that an even slower compund procedure for
+;; computing a Fibonacci sequence can exist.
+(define fibonacci-slower
+  (lambda (n)
+    (if (positive-integer? n)
+        (cond ((equal? n 1) '(0))
+              ((equal? n 2) '(0 1))
+              (else (append (fibonacci-slower (sub1 n))
+                            (list (+ (list-ref (reverse (fibonacci-slower (sub1 n))) 0)
+                                     (list-ref (reverse (fibonacci-slower (sub1 n))) 1)))))))))
+
+
+;; This is a fast implementation for computing a Fibonacci sequence. Since it
+;; relies on the Binet formula, the number of entries which can be computed is
+;; limited by float overflow.
+(define fibonacci-fast
+  (lambda (n)
+    (if (positive-integer? n)
+	(map binet (sequence 0 (sub1 n))))))
+
+
+;; Chosen implementation
+(define fibonacci
+  (lambda (n)
+    (fibonacci-fast n)))
+
+
+;; ROOT FINDING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define illinois
+  (lambda (f x1 x2 . args)
+    (let ((n-iter (if (null? args)
+		      1000
+		      (car args)))
+	  (tol (if (or (null? args)
+		       (null? (cdr args)))
+		   1e-16
+		   (cadr args))))
+      (let* ((f1 (f x1))
+	     (f2 (f x2))
+	     (xc (/ (- (* x1 f2) (/ (* x2 f1) 2))
+		    (- f2 (/ f1 2))))
+	     (fc (f xc)))
+	(if (>= n-iter 1)
+	    (if (> (abs fc) tol)
+		(if (< (* (f xc) f2) 0)
+		    (illinois f xc x2 (sub1 n-iter) tol)
+		    (illinois f x1 xc (sub1 n-iter) tol))
+		(cons (exact->inexact xc)
+		      (cons n-iter
+			    (cons (exact->inexact fc) '())))))))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; STRING MANIPULATION
+;;            (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define empty-string?
+  (lambda (x)
+    (equal? x "")))
+
+
+(define not-empty-string?
+  (lambda (x)
+    (not (empty-string? x))))
+
+
+;; There is already string-join
+(define merge-strings
+  (lambda (strings . args)
+    (let* ((merging-char (if (not (null? args))
+			    (car args)
+			    ""))
+	   (tail (cdr strings))
+	   (last-element? (equal? (cdr tail) '()))
+	   (head (string-append (car strings)
+				merging-char)))
+      (string-append head
+		     (if last-element?
+			 (car tail)
+			 (merge-strings tail merging-char))))))
+
+
+(define char-in-string?
+  (lambda (char string)
+    (> (length (char-positions-in-string char string)) 0)))
+
+
+(define raw-string->number
+  (lambda (string)
+    (string->number
+     (if (char-in-string? #\tab
+			  string)
+	 (list->string (parse-line (if (char-in-string? #\newline)
+				       (list->string (parse-line string
+								 #\newline))
+				       string)
+				   #\tab))
+	 string))))
+
+
+(define my-substring?
+  (lambda (a b)
+    (let* ((a-length (string-length a))
+	   (b-length (string-length b))
+	   (test (> b-length a-length)))
+      (if test
+	  (if (equal? a (substring b 0 a-length))
+	      #t
+	      (substring? a (list->string (cdr (string->list b)))))
+	  #f))))
+
+(define case-insensitive-substring?
+  (lambda (a b)
+    (my-substring? (string-downcase a) (string-downcase b))))
+
+
+(define string-alter
+  (lambda (string alteration-function)
+    (list->string (map alteration-function
+                       (string->list string)))))
+
+
+(define string-upcase
+  (lambda (string)
+    (string-alter string char-upcase)))
+
+
+(define string-downcase
+  (lambda (string)
+    (string-alter string char-downcase)))
+
+
+(define case-insensitive-substring?
+  (lambda (a b)
+    (substring? (string-downcase a)
+                         (string-downcase b))))
+
+
+(define split-string-by-indexes
+  (lambda (string indexes . args)
+    (let* ((splitter (car indexes))
+	   (suppress-splitter (if (null? args)
+				  #t
+				  (car args)))
+           (head (substring string
+                            0
+			    (if suppress-splitter
+				splitter
+				(+ splitter 1))))
+           (tail (substring string
+			    (+ splitter 1)
+			    (string-length string))))
+      (cons head
+            (if (equal? (length indexes) 1)
+                (cons tail '())
+                (split-string-by-indexes tail
+					 (map (lambda (x)
+						(- x
+						   (string-length head)
+						   (if suppress-splitter
+						       1
+						       0)))
+					      (cdr indexes))
+					 suppress-splitter))))))
+
+
+(define find-char-in-string
+  (lambda (char string . args)
+    (let* ((char-list (string->list string))
+	   (list-length (length char-list))
+	   (master-length (if (null? args)
+			      list-length
+			      (car args))))
+      (cons (if (char=? (car char-list) char)
+		(- master-length list-length))
+	    (if (> list-length 1)
+		(find-char-in-string char
+				     (list->string (cdr char-list))
+				     master-length)
+		'())))))
+
+(define char-positions-in-string
+  (lambda (char string)
+    (filter number?
+	    (find-char-in-string char string))))
+
+
+(define string-list->string
+  (lambda (string-list . args)
+    (cond ((null? string-list)
+	   "")
+	  ((pair? (car string-list))
+	   (string-list->string
+	    (map string-list->string string-list)))
+	  (else
+	   (if (null? args)
+	       (set! args (car string-list)))
+	   (string-append (if (equal? (car string-list)
+				      args)
+			      "("
+			      " ")
+			  (car string-list)
+			  (string-list->string (cdr string-list)
+					       args)
+			  (if (equal? (cdr string-list)
+				      '())
+			      ")"
+			      ""))))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; LIST MANIPULATION
+;;          (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;;;
+
+(define improper-list?
+  (lambda (x)
+    (and (not (equal? (car x) '()))
+	 (not (pair? (cdr x))))))
+
+
+(define list?
+  (lambda (x)
+    (and (pair? x)
+	 (not (improper-list? x)))))
+
+
+(define transpose
+  (lambda (structured-list)
+    (apply map list structured-list)))
+
+
+(define cons-cell->list
+  (lambda (cell)
+    (list (car cell)
+	  (cdr cell))))
+
+
+;; TODO Extend in order to deal with improper lists (cons cells)
+(define flatten
+  (lambda (input-list)
+    (cond ((null? input-list) '())
+          ((list? (car input-list))
+           (append (flatten (car input-list))
+                   (flatten (cdr input-list))))
+          (else (cons (car input-list)
+		      (flatten (cdr input-list)))))))
+
+(define sublist
+  (lambda (lst indexes)
+    (if (pair? indexes)
+        (cons (list-ref lst (car indexes))
+              (sublist lst (cdr indexes)))
+        '())))
+
+
+(define slice
+  (lambda (lst first . args)
+    (let ((last (if (null? args)
+                    (sub1 (length lst))
+                    (sub1 (car args)))))
+      (sublist lst
+               (sequence first
+                         last)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; VECTORS AND ARRAYS
+;;           (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define vector-append
+  (lambda (vect ext)
+    (let ((head (vector->list vect))
+          (tail (if (number? ext)
+                    (list ext)
+                    (vector->list ext))))
+      (list->vector (append head tail)))))
+
+
+;; In the case of vectors it is reasonable to assume that only a contiguous
+;; subset of its elements can be of any use
+;; Already defined in CHICKEN - Fluent Scheme to be checked
+(define subvector
+  (lambda (vec first last)
+    (list->vector (slice (vector->list vec)
+                         first
+                         last))))
+
+
+;; Still not working, investigation needed
+(define access-array
+  (lambda (array index . args)
+    (if (vector? array)
+        (if (null? args)
+            (vector-ref array index)
+            (access-array (vector-ref array
+                                      index)
+                          (car args)
+			  (when (not (null? (cdr args)))
+                            (cdr args))))
+        array)))
+
+
+;;;;;;;;;;;;;;
+;; BASE I/O
+;; (REUSABLE)
+;;;;;;;;;;;;;;
+
+(define import-raw-text
+  (lambda (file-name)
+    (with-input-from-file file-name
+	(lambda ()
+	  (let reading ((chars '()))
+	    (let ((char (read-char)))
+	      (if (eof-object? char)
+		  (reverse chars)
+		  (reading (cons char chars)))))))))
+
+
+(define to-file
+  (lambda (output file-name mode)
+    (let ((output-port (open-output-file file-name
+					 mode)))
+      (for-each (lambda (line)
+		  (newline output-port)
+		  (display line
+			   output-port))
+		output)
+      (newline output-port)
+      (close-output-port output-port))))
+
+
+(define write-to-file
+  (lambda (output file-name)
+    (to-file output file-name "w")))
+
+
+(define append-to-file
+  (lambda (output file-name)
+    (to-file output file-name "a")))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DATA IMPORT / EXPORT / TRANSLATION
+;;                           (REUSABLE)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define parse-line
+  (lambda (line delimiter . args)
+    (split-string-by-indexes line
+			     (char-positions-in-string delimiter
+						       line)
+			     (if (null? args)
+				 '()
+				 (car args)))))
+
+
+(define parse-csv-line
+  (lambda (line)
+    (parse-line line #\,)))
+
+
+(define read-lines
+  (lambda (file-name)
+    (parse-line (list->string (import-raw-text file-name))
+		#\newline)))
